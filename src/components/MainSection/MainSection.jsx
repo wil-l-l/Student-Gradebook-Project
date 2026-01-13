b54cc3bad83a1245be8d517a81b396a04910b051
wil-l-l <wsaintlouiswil@gmail.com>
2026-01-12 22:28:43 -0500

    Added border to MainSection and added QuartersBar component to go on top of the GradesTable

--- src/components/MainSection/MainSection.jsx
+++ src/components/MainSection/MainSection.jsx
@@ -1,11 +1,13 @@
-import "./MainSection.css"
+import "./MainSection.css";
 import GradeTable from "../GradeTable/GradeTable";
 import ProfileBox from "../ProfileBox/ProfileBox";
+import QuartersBar from "../QuartersBar/QuartersBar";
 
 const MainSection = () => {
   return (
     <main className="main-section">
       <ProfileBox />
+      <QuartersBar />
       <GradeTable />
     </main>
   );
