66a15c3276c760e1d1a05ffcaca1a88dd2fa05ef
wil-l-l <wsaintlouiswil@gmail.com>
2026-01-14 17:58:08 -0500

    Added an AbortController to student grade data fetching to prevent race conditions

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import "./Reset.css";
 import "./App.css";
 import MainSection from "./components/MainSection/MainSection";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 function App() {
   const [studentInfo, setStudentInfo] = useState(null);
+  const abortControllerRef = useRef(null);
 
-  const API_URL = "http://localhost:3500/students";
+  const API_URL = "http://localhost:3000/students";
   useEffect(() => {
     const fetchGrades = async () => {
+      if (abortControllerRef.current) abortControllerRef.current.abort();
+      abortControllerRef.current = new AbortController();
+
       try {
-        const response = await fetch(API_URL + "/2");
+        const response = await fetch(API_URL + "/2", {
+          signal: abortControllerRef.current.signal,
+        });
         const studentInfo = await response.json();
         setStudentInfo(studentInfo.info);
       } catch (err) {
