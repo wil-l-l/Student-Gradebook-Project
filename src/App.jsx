49d3251269f0cae3674ba9f668db601b7b27e147
wil-l-l <wsaintlouiswil@gmail.com>
2026-01-25 02:44:04 -0500

    Create 'useGrades' custom hook to fetch a student's information

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,12 @@ import "./Reset.css";
 import "./App.css";
 import Header from "./components/Header/Header";
 import MainSection from "./components/MainSection/MainSection";
-import { useEffect, useState, useRef } from "react";
+import { useState } from "react";
+import useGrades from "./hooks/useGrades";
 
 function App() {
   const [editMode, setEditMode] = useState(null);
-  const [studentInfo, setStudentInfo] = useState(null);
-  const [error, setError] = useState(null);
-  const abortControllerRef = useRef(null);
-
-  const API_URL = "http://localhost:3000/students";
-  useEffect(() => {
-    const fetchGrades = async () => {
-      if (abortControllerRef.current) abortControllerRef.current.abort();
-      abortControllerRef.current = new AbortController();
-
-      try {
-        const response = await fetch(API_URL + "/2", {
-          signal: abortControllerRef.current.signal,
-        });
-        const studentInfo = await response.json();
-        setStudentInfo(studentInfo.info);
-      } catch (err) {
-        if (err.name === "AbortError") return;
-        setError("Error Loading Data!");
-      }
-    };
-    fetchGrades();
-  }, []);
+  const { studentInfo, setStudentInfo, error } = useGrades("/2");
 
   return (
     <>
---
+++ src/hooks/useGrades.jsx
@@ -0,0 +1,31 @@
+import { useRef, useEffect, useState } from "react";
+
+const useGrades = (pathParam) => {
+  const [studentInfo, setStudentInfo] = useState(null);
+  const [error, setError] = useState(null);
+  const abortControllerRef = useRef(null);
+
+  const BASE_URL = "http://localhost:3000/students";
+  useEffect(() => {
+    const fetchGrades = async () => {
+      if (abortControllerRef.current) abortControllerRef.current.abort();
+      abortControllerRef.current = new AbortController();
+
+      try {
+        const response = await fetch(BASE_URL + pathParam, {
+          signal: abortControllerRef.current.signal,
+        });
+        const studentInfo = await response.json();
+        setStudentInfo(studentInfo.info);
+      } catch (err) {
+        if (err.name === "AbortError") return;
+        setError("Error Loading Data!");
+      }
+    };
+    fetchGrades();
+  }, [pathParam]);
+
+  return { studentInfo, setStudentInfo, error };
+};
+
+export default useGrades;
