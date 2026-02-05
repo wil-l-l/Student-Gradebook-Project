4bd4f561d8d06e4b0b6c89f0ffc1c73be7db13b6
wil-l-l <wsaintlouiswil@gmail.com>
2026-02-05 17:30:17 -0500

    Remove 'QuartersBar' component and any imports of the component.

--- src/components/MainSection/MainSection.jsx
+++ src/components/MainSection/MainSection.jsx
@@ -1,12 +1,10 @@
 import "./MainSection.css";
 import GradeTable from "../GradeTable/GradeTable";
-import QuartersBar from "../QuartersBar/QuartersBar";
 
 const MainSection = ({ studentInfo, setStudentInfo, editMode }) => {
   return (
     <main className="main-section">
       <>
-        <QuartersBar />
         <GradeTable
           studentInfo={studentInfo}
           editMode={editMode}
